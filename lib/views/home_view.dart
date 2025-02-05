26dcabae77b8e9b73bf4d61484aaf6747706eccf
therealproblem <jiahao10594@hotmail.com>
2025-02-06 04:11:27 +0800

    Setup folder structure, added a simple view and view model

---
+++ lib/views/home_view.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:lxp_flutter_dart/view_models/home_view_model.dart';
+
+class HomeView extends StatefulWidget {
+  const HomeView({super.key, required this.viewModel});
+
+  final HomeViewModel viewModel;
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  @override
+  Widget build(BuildContext context) {
+    return ListenableBuilder(
+      listenable: widget.viewModel,
+      builder: (context, _) {
+        return Scaffold(
+          appBar: AppBar(
+            title: const Text('Home'),
+          ),
+          body: Placeholder(),
+        );
+      },
+    );
+  }
+}
